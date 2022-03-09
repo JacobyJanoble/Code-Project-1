9e7f890af60bb32652dd5f5e770b33f9345390a7
Jacob <Jacob.McCann214@gmail.com>
2022-03-09 11:10:25 -0600

    second commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// const baseUrl = "http://localhost:3000";
+
+// fetch(baseUrl + `/mountains/${Math.floor(Math.random() * 5) + 1}`)
+//   .then((res) => res.json())
+//   .then((mountainData) => renderMountain(mountainData));
+
+// function renderMountain(mountain) {
+//   const image = document.querySelector("#mountain-image");
+//   const name = document.querySelector("#mountain-name");
+//   const location = document.querySelector("#mountain-location");
+//   const likes = document.querySelector("#mountain-likes");
+
+//   image.src = mountain.image;
+//   name.innerText = mountain.name;
+//   location.innerText = mountain.location;
+//   likes.innerText = mountain.likes;
+// }
+
+
+
+// const numItemsToGenerate = 1;
+
+// function renderItem(){
+//     fetch(`https://source.unsplash.com/1600x900/?beach`).then((response) => {
+//       let item = document.createElement('div');
+//       item.classList.add('item');
+//       item.innerHTML = `<img class="beach-image" src="${response.url}" alt="beach image"/>`
+//       document.body.appendChild(item);
+//     })
+//   }
+//   for(let i=0;i<numItemsToGenerate;i++){
+//     renderItem();
+//   }
+
+const test = 1
